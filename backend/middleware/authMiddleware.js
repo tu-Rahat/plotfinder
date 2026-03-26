51624cbec5acdd03c352e5771590e904da396710
tu-Rahat <toslimuddinrahat@gmail.com>
2026-03-27 02:30:06 +0600

    middleware fixed

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+
+const protect = (req, res, next) => {
+  let token;
+
+  const authHeader = req.headers.authorization;
+
+  if (authHeader && authHeader.startsWith("Bearer ")) {
+    token = authHeader.split(" ")[1];
+  }
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Not authorized, no token provided",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Not authorized, invalid token",
+    });
+  }
+};
+
+module.exports = { protect };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,14 @@
+const { protect } = require("../middleware/authMiddleware");
 const express = require("express");
 const router = express.Router();
 const { registerUser, loginUser } = require("../controllers/authController");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-
+router.get("/me", protect, (req, res) => {
+  res.status(200).json({
+    message: "Protected route working",
+    user: req.user,
+  });
+});
 module.exports = router;
